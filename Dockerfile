2faeac95e649abe138d94d63430e30dbfe8eae9f
ZiWeiter <201512990@qq.com>
2021-04-22 10:14:16 +0800

    更新docker配置

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM nginx:alpine
 
-COPY ./dist/ /usr/share/nginx/html
+COPY ./www/ /usr/share/nginx/html
 COPY ./default.conf /etc/nginx/conf.d/default.conf
 COPY ./nginx.conf /etc/nginx/nginx.conf
 
--- dockerBuild.sh
+++ dockerBuild.sh
@@ -2,5 +2,5 @@
 
 rm -rf ./www
 npm run build-server
-docker build -t registry.cn-shenzhen.aliyuncs.com/xmzt_docker/iot-mana-ui:1.0.0 .
-docker push registry.cn-shenzhen.aliyuncs.com/xmzt_docker/iot-mana-ui:1.0.0
+docker build -t registry.cn-hangzhou.aliyuncs.com/llchub/api/task-delivery-buyer-vue:1.0.0 .
+docker push registry.cn-hangzhou.aliyuncs.com/llchub/api/task-delivery-buyer-vue:1.0.0
