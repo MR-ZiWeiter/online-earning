6d6482c63561685c106dde82f244048347af2078
ZiWeiter <201512990@qq.com>
2021-04-29 18:39:34 +0800

    更新配置

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -2,5 +2,5 @@
 
 rm -rf ./www
 npm run build-server
-docker build -t registry.cn-hangzhou.aliyuncs.com/llchub/api/task-delivery-buyer-vue:1.0.0 .
-docker push registry.cn-hangzhou.aliyuncs.com/llchub/api/task-delivery-buyer-vue:1.0.0
+docker build -t registry.cn-hangzhou.aliyuncs.com/llchub/task-delivery-buyer-vue:1.0.0 .
+docker push registry.cn-hangzhou.aliyuncs.com/llchub/task-delivery-buyer-vue:1.0.0
